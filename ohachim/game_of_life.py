2dca33cc655963a1e894befa60d8f0370ca14789
azouiten <azouiten.mail@gmail.com>
2023-10-21 23:00:36 +0100

    fixed bug

--- ohachim/game_of_life.py
+++ ohachim/game_of_life.py
@@ -21,35 +21,36 @@ def map_cells_to_cell_array(cell_array, cells, start_x, start_y):
 def get_live_neighbours(cell_array, x, y):
     start_y =  y - 1 if y - 1 >= 0 else y
     start_x = x - 1 if x - 1 >= 0 else x
-    sx = x - start_x + 1
-    sy = y - start_y + 1
-    print(sy, start_y)
+    sx = x + 2 if x + 2 <= len(cell_array) else x + 1
+    sy = y + 2 if y + 2 <= len(cell_array[0]) else y + 1
     live_neighbours = 0
-    print(cell_array[49][49], "printed")
-    for i in range(start_x, start_x + sx):
-        for j in range(start_y, start_y + sy):
-            print(i, j, cell_array[i][j], x, y)
+    for i in range(start_x, sx):
+        for j in range(start_y, sy):
             if (i != x or j != y) and cell_array[i][j]:
-                print("we come in")
                 live_neighbours += 1
     return live_neighbours
 
 def death_rule(cell_array, x, y):
-    neighbours = 0
-    cols = len(cell_array[0])
-    rows = len(cell_array)
-    life_neighbours = get_live_neighbours(cell_array, x, y)
-    return (neighbours >= 3)
+    live_neighbours = get_live_neighbours(cell_array, x, y)
+    return (live_neighbours == 3)
+
+def survive_rule(cell_array, x, y):
+    live_neighbours = get_live_neighbours(cell_array, x, y)
+    return (live_neighbours == 3 or live_neighbours == 2)
 
 def apply_rules(cell_array):
     cell_array_dup = [[False for x in range(len(cell_array[0]))] for y in range(len(cell_array))]
     for nx, x in enumerate(cell_array):
         for ny, y in enumerate(cell_array[0]):
-            if not y and death_rule(cell_array, nx, ny):
+            if not cell_array[nx][ny] and death_rule(cell_array, nx, ny):
                 cell_array_dup[nx][ny] = True
+            elif cell_array[nx][ny] and survive_rule(cell_array, nx, ny):
+                cell_array_dup[nx][ny] = True
+            else:
+                cell_array_dup[nx][ny] = False
     return cell_array_dup
 
-cells = cells_gen('spaceship1.cells')
+cells = cells_gen('glider.cells')
 # pygame setup
 HEIGHT = 1000
 WIDTH = 1000
@@ -60,17 +61,18 @@ running = True
 step = 20
 cell_array = [[False for x in range(WIDTH // step)] for y in range(HEIGHT // step)]
 cell_array[20][20] = True
+cell_array[19][19] = True
+cell_array[19][21] = True
 cell_array[21][20] = True
-cell_array[22][20] = True
-print(get_live_neighbours(cell_array, 0, 0), "livelve")
-sys.exit()
-# map_cells_to_cell_array(cell_array, cells, 30, 10)
+map_cells_to_cell_array(cell_array, cells, 30, 10)
+print(survive_rule(cell_array, 21, 20), "livelve")
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
 
     screen.fill((65, 62, 67))
     ys = 0
@@ -99,7 +101,7 @@ while running:
 
     # flip() the display to put your work on screen
     pygame.display.flip()
-    pygame.time.wait(1000)
+    pygame.time.wait(500)
     cell_array = apply_rules(cell_array)
 
     clock.tick(60)  # limits FPS to 60
